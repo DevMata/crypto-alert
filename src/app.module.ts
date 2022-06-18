783c47398284d3f8f6043d7fec563e88f85972a2
Antonio Mata <antonio.mata.sv@gmail.com>
2022-06-17 20:00:04 -0600

    set mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlertsModule } from './alerts/alerts.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AlertsModule],
+  imports: [
+    AlertsModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
