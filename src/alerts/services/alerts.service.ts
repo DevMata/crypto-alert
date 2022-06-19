90d13706b26a9e737a781fc0f6898926d02f5785
Antonio Mata <antonio.mata.sv@gmail.com>
2022-06-18 21:46:48 -0600

    add createAlert logic

--- src/alerts/services/alerts.service.ts
+++ src/alerts/services/alerts.service.ts
@@ -2,10 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Alert, AlertDocument } from '../database/alert.schema';
 import { Model } from 'mongoose';
+import { plainToInstance } from 'class-transformer';
+import { CreateAlertDto } from '../dtos/request/create-alert.dto';
+import { AlertDto } from '../dtos/response/alert.dto';
 
 @Injectable()
 export class AlertsService {
   constructor(
     @InjectModel(Alert.name) private readonly alertModel: Model<AlertDocument>,
   ) {}
+
+  async createAlert(createAlertDto: CreateAlertDto): Promise<AlertDto> {
+    const newAlert = new this.alertModel({ ...createAlertDto });
+    await newAlert.save();
+
+    return plainToInstance(AlertDto, newAlert);
+  }
 }
