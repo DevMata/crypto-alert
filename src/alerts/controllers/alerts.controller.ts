b5a4e11aa1db14229f4566a852566b321bbdaf04
Antonio Mata <antonio.mata.sv@gmail.com>
2022-06-20 00:03:03 -0600

    implement OpenAPI

--- src/alerts/controllers/alerts.controller.ts
+++ src/alerts/controllers/alerts.controller.ts
@@ -3,7 +3,9 @@ import { AlertDto } from '../dtos/response/alert.dto';
 import { CreateAlertDto } from '../dtos/request/create-alert.dto';
 import { AlertsService } from '../services/alerts.service';
 import { AlertsValidationDto } from '../dtos/request/alerts-validation.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('alerts')
 @Controller('alerts')
 export class AlertsController {
   constructor(private readonly alertsService: AlertsService) {}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AlertsModule } from './alerts/alerts.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Crypto-Alerts')
+    .setDescription('The cryptocurrency alerts API')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [AlertsModule],
+  });
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap().catch(console.log);
